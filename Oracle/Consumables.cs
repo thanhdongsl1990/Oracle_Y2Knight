5b0f181c6d57ca1c0fabd647016a38ae56d2af49
Kurisuu <Kurisuu@users.noreply.github.com>
2014-11-09 10:26:53 -0800

    Fixed consumables and they now work on minions.

--- Oracle/Consumables.cs
+++ Oracle/Consumables.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using LeagueSharp;
 using LeagueSharp.Common;
 using OC = Oracle.Program;
@@ -15,123 +16,72 @@ namespace Oracle
             Game.OnGameUpdate += Game_OnGameUpdate;       
             Main = new Menu("Consumables", "imenu");
 
-            Menu RegenBiscuit = new Menu("Biscuit", "dbiscuit");
-            RegenBiscuit.AddItem(new MenuItem("useBiscuit", "Use Biscuit")).SetValue(true);
-            RegenBiscuit.AddItem(new MenuItem("bHeathPct", "Use on min HP %")).SetValue(new Slider(40, 1));
-            RegenBiscuit.AddItem(new MenuItem("bManaPct", "Use on Mana %")).SetValue(new Slider(35, 1));
-            RegenBiscuit.AddItem(new MenuItem("bHealthDmg", "Use on Dmg %")).SetValue(new Slider(25, 1));
-            Main.AddSubMenu(RegenBiscuit);
-
-            Menu HealthPotions = new Menu("Health Potions", "hpotions");
-            HealthPotions.AddItem(new MenuItem("useHealth", "Use HealthPot")).SetValue(true);
-            HealthPotions.AddItem(new MenuItem("useHealthPct", "Use on HP %")).SetValue(new Slider(40, 1));
-            HealthPotions.AddItem(new MenuItem("useHealthDmg", "Use on Dmg %")).SetValue(new Slider(25, 1));
-            Main.AddSubMenu(HealthPotions);
-
-            Menu ManaPotions = new Menu("Mana Potions", "mpotions");
-            ManaPotions.AddItem(new MenuItem("useMana", "Use ManaPot")).SetValue(true);
-            ManaPotions.AddItem(new MenuItem("useManaPct", "Use in min Mana %")).SetValue(new Slider(40, 1));
-            Main.AddSubMenu(ManaPotions);
-
-            Menu Crystaline = new Menu("Crystaline Flask", "cflask");
-            Crystaline.AddItem(new MenuItem("useFlask", "Use Flask")).SetValue(true);
-            Crystaline.AddItem(new MenuItem("cHeathPct", "Use on HP %")).SetValue(new Slider(40, 1));
-            Crystaline.AddItem(new MenuItem("cManaPct", "Use on Mana %")).SetValue(new Slider(35, 1));
-            Crystaline.AddItem(new MenuItem("cHealthDmg", "Use on Dmg %")).SetValue(new Slider(35, 1));
-            Main.AddSubMenu(Crystaline);
+            CreateMenuItem("Biscuit", "Biscuit", 40, 25, true, true);
+            CreateMenuItem("Mana Potion", "Mana", 40, 0);
+            CreateMenuItem("Health Potion", "Health", 40, 25, false, true);
+            CreateMenuItem("Crystaline Flask", "Flask", 40, 35, true, true);
 
             Root.AddSubMenu(Main);
         }
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            if (Items.HasItem(2041) && Items.CanUseItem(2041))
-            {
-                if (!Me.HasBuff("Health Potion") || !Me.HasBuff("Mana Potion"))
-                {
-                    var iPercent = (int) ((OC.IncomeDamage/Me.MaxHealth)*100);
-                    var mManaPercent = (int) ((Me.Mana/Me.MaxMana)*100);
-                    var mHealthPercent = (int) ((Me.Health/Me.MaxHealth)*100);                  
-                    if (mHealthPercent <= Main.Item("cHealthPct").GetValue<Slider>().Value &&
-                        Main.Item("useFlask").GetValue<bool>())
-                    {
-                        if (!Me.HasBuff("Recall") && !Me.HasBuff("ItemCrystalFlask") && !Utility.InFountain())
-                             if ((iPercent >= 1 || OC.IncomeDamage >= Me.Health || Me.HasBuffOfType(BuffType.Damage)) && 
-                                OC.AggroTarget.NetworkId == Me.NetworkId)
-                                    Items.UseItem(2041);
-                    }
-                    else if (mManaPercent <= Main.Item("cManaPct").GetValue<Slider>().Value &&
-                             Main.Item("useFlask").GetValue<bool>())
-                    {
-                        if (!Me.HasBuff("Recall") && !Me.HasBuff("ItemCrystalFlask") && !Utility.InFountain())
-                            Items.UseItem(2041);
-                    }
-                    else if (iPercent >= Main.Item("cHealthDmg").GetValue<Slider>().Value)
-                    {
-                         if ((iPercent >= 1 || OC.IncomeDamage >= Me.Health || Me.HasBuffOfType(BuffType.Damage)) && 
-                            OC.AggroTarget.NetworkId == Me.NetworkId)
-                             Items.UseItem(2041);
-
-                    }
-                }
-            }
-            if (Items.HasItem(2004) && Items.CanUseItem(2004))
-            {
-                if (Me.HasBuff("ItemCrystalFlask")) 
-                    return;
-
-                UseHealthPot(OC.IncomeDamage);
-
-                var mManaPercent = (int) ((Me.Mana/Me.MaxMana)*100);
-                if (mManaPercent <= Main.Item("useManaPct").GetValue<Slider>().Value &&
-                    Main.Item("useMana").GetValue<bool>())
-                {
-                    if (!Me.HasBuff("Recall") && !Me.HasBuff("Mana Potion") && !Utility.InFountain())
-                        Items.UseItem(2004);
-                }
-            }
+            UseItem("ItemMiniRegenPotion", "Biscuit", OC.MinionDamage, OC.IncomeDamage);
+            UseItem("ItemCrystalFlask", "Flask", OC.MinionDamage, OC.IncomeDamage);
+            UseItem("FlaskofCrystalWater", "Mana", 0, OC.IncomeDamage, false);
+            UseItem("RegenerationPotion", "Health", OC.MinionDamage, OC.IncomeDamage, true, false);
         }
 
-        private static void UseHealthPot(float incdmg = 0)
+        private static void UseItem(string name, string menuvar, float incdmg = 0, float mindmg = 0, bool usehealth = true, bool usemana = true)
         {
-            var mHealthPercent = (int)((Me.Health / Me.MaxHealth) * 100);
-            var iPercent = (int) ((incdmg/Me.MaxHealth)*100);
-            if (Main.Item("useHealth").GetValue<bool>() && Items.HasItem(2003) && Items.CanUseItem(2003))
-            {
-                if ((mHealthPercent <= Main.Item("useHealthPct").GetValue<Slider>().Value))
-                {
-                    if (!Me.HasBuff("Recall") && !Me.HasBuff("Health Potion") && !Utility.InFountain())
-                    {
-                        if ((iPercent >= 1 || incdmg >= Me.Health || Me.HasBuffOfType(BuffType.Damage)) && 
-                            OC.AggroTarget.NetworkId == Me.NetworkId)
-                                Items.UseItem(2003);
-                    }
-                }
+            if (Me.HasBuff(name) || Me.HasBuff("Recall") || !Items.HasItem(name))
+                return;
+            if (!Main.Item("use" + menuvar).GetValue<bool>())
+                return;
+            var consumable = Me.GetSpellSlot(name);
+            if (consumable == SpellSlot.Unknown)
+                return;
+            var consumableslot = new Spell(consumable);
+            if (!consumableslot.IsReady())
+                return;
 
-                else if (iPercent >= Main.Item("useHealthDmg").GetValue<Slider>().Value)
-                {
-                    if ((iPercent >= 2 || incdmg >= Me.Health) && 
-                        OC.AggroTarget.NetworkId == Me.NetworkId)
-                            Items.UseItem(2003);
-                }
-            }
-            else if (Main.Item("useBiscuit").GetValue<bool>() && Items.HasItem(2009) && Items.CanUseItem(2009))
+            var aHealthPercent = (int)((Me.Health / Me.MaxHealth) * 100);
+            var aManaPercent = (int) ((Me.Mana/Me.MaxMana)*100);
+            var iDamagePercent = (int) ((incdmg/Me.MaxHealth)*100);
+            var mDamagePercent = (int) ((mindmg/Me.MaxHealth)*100);
+                
+            if (usehealth && aHealthPercent <= Main.Item("use" + menuvar + "Pct").GetValue<Slider>().Value)
             {
-                if ((mHealthPercent <= Main.Item("bHeathPct").GetValue<Slider>().Value))
-                {
-                    if (!Me.HasBuff("Recall") && !Me.HasBuff("Health Potion") && !Utility.InFountain())
-                    {
-                        if ((iPercent >= 2 || incdmg >= Me.Health) && OC.AggroTarget.NetworkId == Me.NetworkId)
-                            Items.UseItem(2009);
-                    }
-                }
+                if (iDamagePercent >= 1 || incdmg >= Me.Health || Me.HasBuff("summonerdot") ||
+                    mDamagePercent >= 1 || mindmg >= Me.Health )
+                    if (OC.AggroTarget.NetworkId == Me.NetworkId)
+                        consumableslot.Cast();
 
-                if (iPercent >= Main.Item("bHealthDmg").GetValue<Slider>().Value)
-                {
-                    if ((iPercent >= 2 || incdmg >= Me.Health) && OC.AggroTarget.NetworkId == Me.NetworkId)
-                            Items.UseItem(2009);
-                }
+                if (iDamagePercent >= Main.Item("use" + menuvar + "Dmg").GetValue<Slider>().Value)
+                    if (OC.AggroTarget.NetworkId == Me.NetworkId)
+                        consumableslot.Cast();
             }
+            else if (usemana && aManaPercent <= Main.Item("use" + menuvar + "Mana").GetValue<Slider>().Value)
+            {
+                if (Me.Mana == 0)
+                    return;
+                consumableslot.Cast();
+            }
+
+        }
+
+        private static void CreateMenuItem(string name, string menuvar, int dvalue, int dmgvalue, bool usemana = true, bool usehealth = false)
+        {
+            Menu menuName = new Menu(name, "m" +menuvar);
+            menuName.AddItem(new MenuItem("use" + menuvar, "Use " + name)).SetValue(true);
+            if (usehealth)
+            {
+                menuName.AddItem(new MenuItem("use" + menuvar + "Pct", "Use on HP &")).SetValue(new Slider(dvalue));
+                menuName.AddItem(new MenuItem("use" + menuvar + "Dmg", "Use on Dmg %")).SetValue(new Slider(dmgvalue));
+            }
+            if (usemana)
+                menuName.AddItem(new MenuItem("use" + menuvar + "Mana", "Use on Mana %")).SetValue(new Slider(40));
+            Main.AddSubMenu(menuName);
         }
     }
 }
