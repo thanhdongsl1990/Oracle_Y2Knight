4cea30db1b01c89afa1c591c7f17b0e8067d958c
Kurisu44 <Kurisu44@users.noreply.github.com>
2014-11-29 00:41:41 -0800

    Revert.

--- Oracle/Consumables.cs
+++ Oracle/Consumables.cs
@@ -17,64 +17,67 @@ namespace Oracle
             mainmenu = new Menu("Consumables", "imenu");
 
             CreateMenuItem("Mana Potion", "Mana", 40, 0);
-            CreateMenuItem("Biscuit", "BiscuitHealthMana", 40, 25);
-            CreateMenuItem("Health Potion", "PotionHealth", 40, 25);
-            CreateMenuItem("Crystaline Flask", "FlaskHealthMana", 40, 35);
+            CreateMenuItem("Biscuit", "Biscuit", 40, 25, true, true);
+            CreateMenuItem("Health Potion", "Health", 40, 25, false, true);
+            CreateMenuItem("Crystaline Flask", "Flask", 40, 35, true, true);
 
             root.AddSubMenu(mainmenu);
         }
 
         private static void Game_OnGameUpdate(EventArgs args)
         {
-            UseItem("ItemCrystalFlask", 2041, "FlaskHealthMana");
-            UseItem("ItemMiniRegenPotion", 2010, "BiscuitHealthMana");
-            UseItem("RegenerationPotion", 2003, "PotionHealth");
-            UseItem("FlaskOfCrystalWater", 2004, "Mana");
+            UseItem("FlaskOfCrystalWater", 2004, "Mana", 0, 0, false);
+            UseItem("ItemMiniRegenPotion", 2010, "Biscuit", OC.IncomeDamage, OC.MinionDamage);
+            UseItem("ItemCrystalFlask", 2041, "Flask", OC.IncomeDamage, OC.MinionDamage);
+            UseItem("RegenerationPotion", 2003, "Health", OC.IncomeDamage, OC.MinionDamage, true, false);
         }
 
-        private static void UseItem(string name, int itemId, string menuvar)
+        private static void UseItem(string name, int itemId, string menuvar, float incdmg = 0, float mindmg = 0, bool usehealth = true, bool usemana = true)
         {
-            var incdmg = OC.IncomeDamage;
-            var mindmg = OC.MinionDamage;
-
             if (!Items.HasItem(itemId) || !Items.CanUseItem(itemId))
                 return;
             
             if (!me.HasBuff(name, true) && me.NotRecalling() && !Utility.InFountain())
             {
-                if (mainmenu.Item("use" + menuvar).GetValue<bool>())
-                {
-                    if (menuvar.Contains("Mana") && me.Mana/me.MaxMana*100 <= mainmenu.Item("use" + menuvar + "Mana").GetValue<Slider>().Value)
-                        Items.UseItem(itemId);
+                if (!mainmenu.Item("use" + menuvar).GetValue<bool>())
+                    return;
 
-                    if (menuvar.Contains("Health") && me.Health/me.MaxHealth*100 <= mainmenu.Item("use" + menuvar + "Pct").GetValue<Slider>().Value)
+                var aHealthPercent = (int) ((me.Health/me.MaxHealth)*100);
+                var aManaPercent = (int) ((me.Mana/me.MaxMana)*100);
+                var iDamagePercent = (int) ((incdmg/me.MaxHealth)*100);
+                var mDamagePercent = (int) ((mindmg/me.MaxHealth)*100);
+
+                if (usemana && aManaPercent <= mainmenu.Item("use" + menuvar + "Mana").GetValue<Slider>().Value)
+                    Items.UseItem(itemId);
+
+                if (usehealth && aHealthPercent <= mainmenu.Item("use" + menuvar + "Pct").GetValue<Slider>().Value)
+                {
+                    if (iDamagePercent >= 1 || incdmg >= me.Health || me.HasBuff("summonerdot") ||
+                        mDamagePercent >= 1 || mindmg >= me.Health)
                     {
                         if (OC.AggroTarget.NetworkId == me.NetworkId)
-                        {
-                            var dmgpct = incdmg/me.MaxHealth*100;
-                            var minpct = mindmg/me.MaxHealth*100;
+                            Items.UseItem(itemId);
+                    }
 
-                            if (dmgpct >= 1 || minpct >= 1 || incdmg >= me.Health || me.HasBuff("summonerdot"))
-                                Items.UseItem(itemId);
-
-                            if (dmgpct >= mainmenu.Item("use" + menuvar + "Dmg").GetValue<Slider>().Value)
-                                Items.UseItem(itemId);
-                        }
+                    else if (iDamagePercent >= mainmenu.Item("use" + menuvar + "Dmg").GetValue<Slider>().Value)
+                    {
+                        if (OC.AggroTarget.NetworkId == me.NetworkId)
+                            Items.UseItem(itemId);
                     }
                 }
             }
         }
 
-        private static void CreateMenuItem(string name, string menuvar, int value, int dmgvalue)
+        private static void CreateMenuItem(string name, string menuvar, int dvalue, int dmgvalue, bool usemana = true, bool usehealth = false)
         {
             var menuName = new Menu(name, "m" + menuvar);
             menuName.AddItem(new MenuItem("use" + menuvar, "Use " + name)).SetValue(true);
-            if (menuvar.Contains("Health"))
+            if (usehealth)
             {
-                menuName.AddItem(new MenuItem("use" + menuvar + "Pct", "Use on HP &")).SetValue(new Slider(value));
+                menuName.AddItem(new MenuItem("use" + menuvar + "Pct", "Use on HP &")).SetValue(new Slider(dvalue));
                 menuName.AddItem(new MenuItem("use" + menuvar + "Dmg", "Use on Dmg %")).SetValue(new Slider(dmgvalue));
             }
-            if (menuvar.Contains("Mana"))
+            if (usemana)
                 menuName.AddItem(new MenuItem("use" + menuvar + "Mana", "Use on Mana %")).SetValue(new Slider(40));
             mainmenu.AddSubMenu(menuName);
         }
--- Oracle/Defensives.cs
+++ Oracle/Defensives.cs
@@ -9,6 +9,7 @@ namespace Oracle
 {
     internal static class Defensives
     {
+        private static int bufftick, bufftime;
         private static bool danger, stealth;
         private static Menu mainmenu, menuconfig;
         private static readonly Obj_AI_Hero me = ObjectManager.Player;
@@ -22,7 +23,7 @@ namespace Oracle
             menuconfig = new Menu("Defensive Config", "dconfig");
 
             foreach (Obj_AI_Hero x in ObjectManager.Get<Obj_AI_Hero>().Where(x => x.IsAlly))
-                menuconfig.AddItem(new MenuItem("duseon" + x.SkinName, "Use for " + x.SkinName)).SetValue(true);
+                menuconfig.AddItem(new MenuItem("DefenseOn" + x.SkinName, "Use for " + x.SkinName)).SetValue(true);
             mainmenu.AddSubMenu(menuconfig);
 
             CreateMenuItem("Randuin's Omen", "Randuins", 40, 40, true);
@@ -62,6 +63,9 @@ namespace Oracle
             // Oracle's Lens
             if (Items.HasItem(3364) && Items.CanUseItem(3364) && mainmenu.Item("useOracles").GetValue<bool>())
             {
+                if (!Items.CanUseItem(3364))
+                    return;
+
                 if (!OC.Origin.Item("ComboKey").GetValue<KeyBind>().Active &&
                     mainmenu.Item("oracleMode").GetValue<StringList>().SelectedIndex == 1)
                     return;
@@ -69,8 +73,8 @@ namespace Oracle
                 if (!stealth)
                     return;
 
-                var target = OC.FriendlyTarget;
-                if (target.Distance(me.Position, true) <= 600*600 && stealth || target.HasBuff("RengarRBuff", true))
+                var target = OC.FriendlyTarget();
+                if (target.Distance(me.Position) <= 600f && stealth || target.HasBuff("RengarRBuff", true))
                     Items.UseItem(3364, target.Position);
             }
 
@@ -96,23 +100,20 @@ namespace Oracle
                     mainmenu.Item("talismanMode").GetValue<StringList>().SelectedIndex == 1)
                     return;
 
-                var target = OC.FriendlyTarget;
+                var target = OC.FriendlyTarget();
                 if (target.Distance(me.Position) > 600)
                     return;
 
-                var enemies = target.CountHerosInRange(1000);
-                var allies = target.CountHerosInRange(1000, false);
+                var enemies = target.CountHerosInRange(true, 1000);
+                var allies = target.CountHerosInRange(false, 1000);
 
                 var weakEnemy =
                     ObjectManager.Get<Obj_AI_Hero>()
                         .OrderByDescending(ex => ex.Health/ex.MaxHealth*100)
-                        .FirstOrDefault(x => x.IsValidTarget(1000));
+                        .First(x => x.IsValidTarget(1000));
 
-                if (weakEnemy == null)
-                    return;
-
-                var aHealthPercent = target.Health/target.MaxHealth*100;
-                var eHealthPercent = weakEnemy.Health/weakEnemy.MaxHealth*100;
+                var aHealthPercent = target.Health / target.MaxHealth * 100;
+                var eHealthPercent = weakEnemy.Health / weakEnemy.MaxHealth * 100;
 
                 if (weakEnemy.Distance(target.Position) <= 900 &&
                     (allies > enemies && eHealthPercent <= mainmenu.Item("useEnemyPct").GetValue<Slider>().Value))
@@ -123,27 +124,26 @@ namespace Oracle
                 if (enemies > allies && aHealthPercent <= mainmenu.Item("useAllyPct").GetValue<Slider>().Value)
                 {
                     Items.UseItem(3069);
-                }      
+                }
             }
 
             // Deffensives
-            if (OC.FriendlyTarget != null)
-            {
-                if (OC.IncomeDamage >= 1)
-                {
-                    UseItem("Locket", 3190, 600f, OC.IncomeDamage);
-                    UseItem("Seraphs", 3040, 450f, OC.IncomeDamage, true);
-                    UseItem("Zhonyas", 3157, 450f, OC.IncomeDamage, true);
-                    UseItem("Randuins", 3143, 450f, OC.IncomeDamage);
-                    UseItem("Mountain", 3401, 700f, OC.IncomeDamage, false, true);
+            if (OC.FriendlyTarget() == null) 
+                return;
+            if (OC.IncomeDamage < 1) 
+                return;
 
-                    if (Game.MapId == GameMapId.CrystalScar)
-                    {
-                        UseItem("Odyns", 3180, 450f, OC.IncomeDamage);
-                        UseItem("Wooglets", 3090, 450f, OC.IncomeDamage, true);
-                    }
-                }
-            }
+            UseItem("Locket", 3190, 600f, OC.IncomeDamage);
+            UseItem("Seraphs", 3040, 450f, OC.IncomeDamage, true);
+            UseItem("Zhonyas", 3157, 450f, OC.IncomeDamage, true);
+            UseItem("Randuins", 3143, 450f, OC.IncomeDamage);
+            UseItem("Mountain", 3401, 700f, OC.IncomeDamage, false, true);
+
+            if (Game.MapId != GameMapId.CrystalScar)
+                return;
+
+            UseItem("Odyns", 3180, 450f, OC.IncomeDamage);
+            UseItem("Wooglets", 3090, 450f, OC.IncomeDamage, true);
         }
 
         private static void UseItem(string name, int itemId, float itemRange, float incdmg = 0, bool selfuse = false, bool targeted = false)
@@ -151,37 +151,46 @@ namespace Oracle
             if (!Items.HasItem(itemId) || !Items.CanUseItem(itemId))
                 return;
 
-            var target = selfuse ? me : OC.FriendlyTarget;
-            if (mainmenu.Item("use" + name).GetValue<bool>() && target.Distance(me.Position) <= itemRange)
-            {            
-                var aHealthPercent = (int) ((target.Health/target.MaxHealth)*100);
-                var iDamagePercent = (int) (incdmg/target.MaxHealth*100);
+            if (!mainmenu.Item("use" + name).GetValue<bool>())
+                return;
+        
+            var target = selfuse ? me : OC.FriendlyTarget();
+            if (target.Distance(me.Position) > itemRange)
+                return;
 
-                if (me.NotRecalling() && mainmenu.Item("duseon" + target.SkinName).GetValue<bool>())
+            var aHealthPercent = (int) ((target.Health/target.MaxHealth)*100);
+            var iDamagePercent = (int) (incdmg/target.MaxHealth*100);
+
+            if (me.NotRecalling() && mainmenu.Item("DefenseOn" + target.SkinName).GetValue<bool>())
+            {
+                if (aHealthPercent <= mainmenu.Item("use" + name + "Pct").GetValue<Slider>().Value)
                 {
-                    if (name == "Randuins" || name == "Odyns")
-                    {
-                        if (target.CountHerosInRange(itemRange) >= 
-                            mainmenu.Item("use" + name + "Count").GetValue<Slider>().Value)
-                                Items.UseItem(itemId, targeted ? target : null);
-                    }
+                    if ((iDamagePercent >= 1 || incdmg >= target.Health) && 
+                        OC.AggroTarget.NetworkId == target.NetworkId)
+                            Items.UseItem(itemId, targeted ? target : null);
 
-                    if (aHealthPercent <= mainmenu.Item("use" + name + "Pct").GetValue<Slider>().Value)
-                    {
-                        if ((iDamagePercent >= 1 || incdmg >= target.Health) &&
-                            OC.AggroTarget.NetworkId == target.NetworkId)
-                                Items.UseItem(itemId, targeted ? target : null);
-
-                        if (iDamagePercent >= mainmenu.Item("use" + name + "Dmg").GetValue<Slider>().Value &&
-                            OC.AggroTarget.NetworkId == target.NetworkId)
-                                Items.UseItem(itemId, targeted ? target : null);
-                    }
-
-                    if (mainmenu.Item("use" + name + "Danger").GetValue<bool>())
-                        if (danger && OC.AggroTarget.NetworkId == target.NetworkId)
+                    if (iDamagePercent >= mainmenu.Item("use" + name + "Dmg").GetValue<Slider>().Value && 
+                        OC.AggroTarget.NetworkId == target.NetworkId)
                             Items.UseItem(itemId, targeted ? target : null);
                 }
+
+                if (mainmenu.Item("use" + name + "Danger").GetValue<bool>())
+                    if (danger && OC.AggroTarget.NetworkId == target.NetworkId)
+                        Items.UseItem(itemId, targeted ? target : null);             
             }
+        }
+
+        private static void CreateMenuItem(string displayname, string name, int hpvalue, int dmgvalue, bool itemcount = false)
+        {
+            var menuName = new Menu(displayname, name.ToLower());
+            menuName.AddItem(new MenuItem("use" + name, "Use " + name)).SetValue(true);
+            menuName.AddItem(new MenuItem("use" + name + "Pct", "Use on HP %")).SetValue(new Slider(hpvalue));
+            if (!itemcount)
+                menuName.AddItem(new MenuItem("use" + name + "Dmg", "Use on Dmg %")).SetValue(new Slider(dmgvalue));
+            if (itemcount)
+                menuName.AddItem(new MenuItem("use" + name + "Count", "Use on Count")).SetValue(new Slider(3, 1, 5));
+            menuName.AddItem(new MenuItem("use" + name + "Danger", "Use on Dangerous")).SetValue(true);
+            mainmenu.AddSubMenu(menuName);
         }
 
         private static void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
@@ -189,7 +198,7 @@ namespace Oracle
             if (!sender.IsEnemy || sender.Type != me.Type)
                 return;
 
-            var target = OC.FriendlyTarget;
+            var target = OC.FriendlyTarget();
             var attacker = ObjectManager.Get<Obj_AI_Hero>().First(x => x.NetworkId == sender.NetworkId);
             var attackerslot = attacker.GetSpellSlot(args.SData.Name);
 
@@ -205,19 +214,6 @@ namespace Oracle
                 if (data.DangerLevel == RiskLevel.Extreme && attackerslot == SpellSlot.R)
                     danger = true;
             }
-        }   
-
-        private static void CreateMenuItem(string displayname, string name, int hpvalue, int dmgvalue, bool itemcount = false)
-        {
-            var menuName = new Menu(displayname, name.ToLower());
-            menuName.AddItem(new MenuItem("use" + name, "Use " + name)).SetValue(true);
-            menuName.AddItem(new MenuItem("use" + name + "Pct", "Use on HP %")).SetValue(new Slider(hpvalue));
-            if (!itemcount)
-                menuName.AddItem(new MenuItem("use" + name + "Dmg", "Use on Dmg %")).SetValue(new Slider(dmgvalue));
-            if (itemcount)
-                menuName.AddItem(new MenuItem("use" + name + "Count", "Use on Count")).SetValue(new Slider(3, 1, 5));
-            menuName.AddItem(new MenuItem("use" + name + "Danger", "Use on Dangerous")).SetValue(true);
-            mainmenu.AddSubMenu(menuName);
-        }   
+        }       
     }
 }
